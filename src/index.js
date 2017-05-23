0215ddbe1be4b98ff5c39c9ac545adcde5f3f960
Kyle Guthrie <vt.guthrie@gmail.com>
2017-05-22 21:23:18 -0700

    Add ReduxPromise for async requests

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import ReduxPromise from 'redux-promise';
 
 
 const configuredStore = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(ReduxPromise, thunk)
 );
 
 ReactDOM.render(
