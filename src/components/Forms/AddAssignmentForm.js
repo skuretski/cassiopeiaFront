0c1ce00331af852d74f3c1ad7ae7b0e719c28d60
Kyle Guthrie <vt.guthrie@gmail.com>
2017-05-26 21:31:46 -0700

    Style assignment form and add validation
    Validation function checks start/end date combinations, as well as if
    the selections are valid. Effort is restricted to [0, 1.0].

--- src/components/Forms/AddAssignmentForm.js
+++ src/components/Forms/AddAssignmentForm.js
@@ -13,56 +13,157 @@ class AddAssignmentForm extends Component {
     renderField(field) {
         const { meta: { touched, error } } = field;
         const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-        return (
-            <div className={className}>
-                <label>{field.label}</label>
-                <input className="form-control" type={field.type} {...field.input}/>
-                <div className="text-help">
-                    {touched ? error : ''}
+        if (field.type === 'select') {
+            return (
+                <div className={className}>
+                    <label>{field.label}</label>
+                    <select className="form-control" {...field.input}>
+                        {field.selectOptions}
+                    </select>
+                    <div className="text-help">
+                        {touched ? error : ''}
+                    </div>
                 </div>
-            </div>
-       );
+            );
+        } else if (field.type === 'number') {
+            return (
+                <div className={className}>
+                    <label>{field.label}</label>
+                    <input className="form-control"
+                        type={field.type}
+                        min={field.min}
+                        step={field.step}
+                        max={field.max}
+                        {...field.input}/>
+                    <div className="text-help">
+                        {touched ? error : ''}
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className={className}>
+                    <label>{field.label}</label>
+                    <input className="form-control" type={field.type} {...field.input}/>
+                    <div className="text-help">
+                        {touched ? error : ''}
+                    </div>
+                </div>
+            );
+        }
     }
 
     onSubmit(values) {
         console.log(values);
     }
 
+    renderEmployeeOptions() {
+        var employees = this.props.employees;
+        var opts = [];
+        opts.push(<option key={0} value={0}>Select Employee...</option>)
+        employees.map( e => {
+            opts.push(<option key={e.id} value={e.id}>{e.last + ', ' + e.first}</option>)
+        })
+        return opts;
+    }
+
+    renderMonthOptions() {
+        var opts = [];
+        opts.push(<option key={0} value={0}>Select Month...</option>)
+        const months = ['January','February','March','April','May','June','July',
+         'August','September','October','November','December'];
+        months.map( (name, num) => {
+            opts.push(<option key={num+1} value={num+1}>{name}</option>)
+        });
+        return opts;
+    }
+
+    renderYearOptions() {
+        var yr = new Date().getFullYear();
+        var opts = [];
+        opts.push(<option key={0} value={0}>Select Year...</option>)
+        for (var i = 0; i <= 10; ++i) {
+            opts.push(<option key={yr+i} value={yr+i}>{yr+i}</option>);
+        }
+        return opts;
+    }
+
     render() {
-        console.log(this.props);
         const { handleSubmit } = this.props;
         return (
             <fieldset>
             <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <label>Employee</label>
-                <div>
-                    <Field name="assignmentEmployee" component="select">
-                        {this.props.employees.map( e => {
-                            return (
-                                <option key={e.id} value={e.id}>{e.last + ', ' + e.first}</option>
-                            )
-                        })}
-                    </Field>
+                <div className="row">
+                    <div className="col-md-2">
+                        {/* Employee Field */}
+                        <Field
+                            label="Employee"
+                            name="assignmentEmployee"
+                            type="select"
+                            selectOptions={this.renderEmployeeOptions()}
+                            component={this.renderField}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        {/* Effort Field */}
+                        <Field
+                            label="Effort"
+                            name="assignmentEffort"
+                            type="number"
+                            min="0.0"
+                            step="0.1"
+                            max="1.0"
+                            component={this.renderField}
+                        />
+                    </div>
                 </div>
-                <Field
-                    label="Effort"
-                    name="assignmentEffort"
-                    type="text"
-                    component={this.renderField}
-                />
-                <Field
-                    label="Start Date"
-                    name="assignmentStartDate"
-                    type="month"
-                    component={this.renderField}
-                />
-                <Field
-                    label="End Date"
-                    name="assignmentEndDate"
-                    type="text"
-                    component={this.renderField}
-                /> 
+
+                <div className="row">
+                    {/* Start Date Fields */}
+                    <div className="col-md-2">
+                        <Field
+                            label="Start Month"
+                            name="assignmentStartMonth"
+                            type="select"
+                            selectOptions={this.renderMonthOptions()}
+                            component={this.renderField}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        <Field
+                            label="Start Year"
+                            name="assignmentStartYear"
+                            type="select"
+                            selectOptions={this.renderYearOptions()}
+                            component={this.renderField}
+                        />
+                    </div>
+                </div>
+
+                {/* End Date Fields */}
+                <div className="row">
+                    <div className="col-md-2">
+                        <Field
+                            label="End Month"
+                            name="assignmentEndMonth"
+                            type="select"
+                            selectOptions={this.renderMonthOptions()}
+                            component={this.renderField}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        <Field
+                            label="End Year"
+                            name="assignmentEndYear"
+                            type="select"
+                            selectOptions={this.renderYearOptions()}
+                            component={this.renderField}
+                        />
+                    </div>
+                </div>
+                    
                 <button type="submit" className="btn btn-primary">Submit</button>
+
             </form>
             </fieldset>
         );
@@ -71,27 +172,57 @@ class AddAssignmentForm extends Component {
 
 function validate(values) {
     const errors = {};
+    //TODO:
+    // * Validate that input start date is in today's month/year or later
+    // * Validate that end date is after start date
+    // * Validate that effort is in range [0, 1]
     // Validate the inputs from 'values'
-    if (!values.assignmentEmployee) {
+    const employee = parseInt(values.assignmentEmployee);
+    const effort = values.assignmentEffort;
+    const startMonth = parseInt(values.assignmentStartMonth);
+    const startYear = parseInt(values.assignmentStartYear);
+    const endMonth = parseInt(values.assignmentEndMonth);
+    const endYear = parseInt(values.assignmentEndYear);
+
+    if (!employee) {
         errors.assignmentEmployee = "Select an employee.";
     }
 
-    if (!values.assignmentEffort) {
+    if (!effort) {
         errors.assignmentEffort = "Enter an effort amount.";
+    } else if (effort < 0.0 || effort > 1.0) {
+        errors.assignmentEffort = "Must be between 0 and 1."
     }
 
-    if (!values.assignmentStartDate) {
-        errors.assignmentStartDate = "Enter a start date.";
+    if (!startMonth) {
+        errors.assignmentStartMonth = "Select a start month.";
     }
 
-    if (!values.assignmentEndDate) {
-        errors.assignmentEndDate = "Enter a end date.";
+    if (!startYear) {
+        errors.assignmentStartYear = "Select a start year.";
     }
 
-    // If errors is empty, the form is fine to submit
-    // If errors has *any* properties, redux-form assumes form is invalid
+    if (!endMonth) {
+        errors.assignmentEndMonth = "Select an end month.";
+    }
+
+    if (!endYear) {
+        errors.assignmentEndYear = "Select an end year.";
+    }
+
+    if (startMonth && startYear && endMonth && endYear) {
+        if (startYear > endYear) {
+            errors.assignmentStartYear = "Start year must be before end year.";
+            errors.assignmentEndYear = "End year must be after start year.";
+        } else if (startYear == endYear) {
+            if (startMonth > endMonth) {
+                errors.assignmentStartMonth = "Start month must be before end month.";
+                errors.assignmentEndMonth = "End month must be after start month.";
+            }
+        }
+    }
+
     return errors;
-
 }
 
 AddAssignmentForm = reduxForm({
