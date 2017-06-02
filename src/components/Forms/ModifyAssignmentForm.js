8f7070cfbdf0a13568b4410114790adb7a861038
Kyle Guthrie <vt.guthrie@gmail.com>
2017-06-02 16:46:33 -0700

    Remove debug console.log() >.<

--- src/components/Forms/ModifyAssignmentForm.js
+++ src/components/Forms/ModifyAssignmentForm.js
@@ -20,14 +20,12 @@ class ModifyAssignmentForm extends Component {
         // Add task_id to the fields values
         this.props.change('assignmentTaskID', this.props.task_id);
         // Get initial employee list
-        console.log('DISCIPLINE: ', this.props.discipline_id);
         this.props.getEmployeesByDiscipline(this.props.discipline_id);
     }
 
     componentWillUpdate(nextProps) {
         // New discipline --> refresh employee list
         if (this.props.discipline_id != nextProps.discipline_id) {
-            console.log('UPDATING...');
             this.props.getEmployeesByDiscipline(nextProps.discipline_id);
         }
     }
